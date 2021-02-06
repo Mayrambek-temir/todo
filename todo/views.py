5761135a1ba7db8f6e598c0e08d71e3ea6b0de8b
Mayrambek-temir <m.temirov2015@gmail.com>
2021-02-06 18:31:30 +0600

    init2

--- todo/views.py
+++ todo/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render, HttpResponse
 
 def pomepage(request):
     return render(request, 'index.html')
+
+def pomepage(request):
+    return render(request, 'index.html')
