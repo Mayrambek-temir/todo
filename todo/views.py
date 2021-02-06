d69559def8fa392c4ac6f956b39e7b66beb7bb9b
Mayrambek-temir <m.temirov2015@gmail.com>
2021-02-06 18:34:27 +0600

    init returned to first status

--- todo/views.py
+++ todo/views.py
@@ -3,5 +3,3 @@ from django.shortcuts import render, HttpResponse
 def pomepage(request):
     return render(request, 'index.html')
 
-def pomepage(request):
-    return render(request, 'index.html')
