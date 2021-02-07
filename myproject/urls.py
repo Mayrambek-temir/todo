55e810605df17ef9748c9cac7b38fd4336c13dd1
Mayrambek-temir <m.temirov2015@gmail.com>
2021-02-07 16:58:34 +0600

    added form and ad data to DB, redirect(test)

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from todo.views import pomepage
-from todo.views import test
+from todo.views import *
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -24,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', pomepage, name="name"),
     path('test/', test, name="test"),
+    path('add-todo/', add_todo, name="add-todo")
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import Todo
 
 def pomepage(request):
@@ -7,3 +7,10 @@ def pomepage(request):
 def test(request):
     Todo_list=Todo.objects.all()
     return render(request, 'test.html', {"Todo_list":Todo_list})
+
+def add_todo(request):
+    form=request.POST
+    text=form["todo_text"]
+    todo=Todo(text=text)
+    todo.save()
+    return redirect(test)
