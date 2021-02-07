2b17374394f1a49304ccdceafaa24795d432e17e
Mayrambek-temir <m.temirov2015@gmail.com>
2021-02-07 15:42:50 +0600

    in html loop for

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from todo.views import pomepage
+from todo.views import test
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', pomepage, name="name"),
+    path('test/', test, name="test"),
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, HttpResponse
+from .models import Todo
 
 def pomepage(request):
     return render(request, 'index.html')
-
+    
+def test(request):
+    Todo_list=Todo.objects.all()
+    return render(request, 'test.html', {"Todo_list":Todo_list})
